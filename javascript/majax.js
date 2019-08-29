1cd9b434b5676f33a5f78561f1fdbf14486db25c
nextchangesoftwares <53474269+nextchangesoftwares@users.noreply.github.com>
2019-08-29 12:21:55 +0530

    Update majax.js
    initiate node.js connect

--- javascript/majax.js
+++ javascript/majax.js
@@ -1,1 +1,13 @@
-function();
+function majax()
+{
+var io = require('socket.io').listen(80); // initiate socket.io server
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' }); // Send data to client
+
+  // wait for the event raised by the client
+  socket.on('my other event', function (data) {  
+    console.log(data);
+  });
+});
+};
